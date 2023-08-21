1256ae7dd85eafcb691dd072ae134d6cd6a4279f
ZENO <zeno1030@naver.com>
2023-08-21 22:19:41 +0900

    DATEDIFF를 사용하자 (날짜 간의 차이를 알고 싶을 때

--- sql_book/case.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-    user_id
-, CASE
-    WHEN register_device = 1  THEN '데스크톱'
-    WHEN register_device = 2  THEN '스마트폰'
-    WHEN register_device = 3  THEN '애플리케이션'
-    --디폴트 값을 지정할 경우 ELSE 구문을 사용
-  END AS device_name
-FROM mst_users
